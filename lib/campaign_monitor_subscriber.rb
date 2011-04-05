202d39d5b33548ee606f67ff088d2dc133e44c65
Gary Greyling <greyling.gary@gmail.com>
2011-04-05 13:40:54 +0200

    simplify module internals, correct spelling

--- lib/campaign_monitor_subscriber.rb
+++ lib/campaign_monitor_subscriber.rb
@@ -1,39 +1,30 @@
 module CampaignMonitorSubscriber
-  CAMPAIGN_MONITOR_API_KEY = YAML::load_file(File.join(RAILS_ROOT, "config/campaign_monitor_subscriber_config.yml"))['api_key']
-  
-  def self.included(base)
-    base.extend ClassMethods
-  end
+  require 'campaigning'
+  ::CAMPAIGN_MONITOR_API_KEY = YAML::load_file(File.join(RAILS_ROOT, "config/campaign_monitor_subscriber_config.yml"))['api_key']  
 
-  module ClassMethods
-    require 'campaigning'
+  def subscribe_me_using(email_field)
+    # return unless RAILS_ENV == 'production'
 
-    def subcribe_me_using(email_field)
-      return unless RAILS_ENV == 'production'
-
-      after_create do |record|
-        begin
-          s = Campaigning::Subscriber.new(record.send(email_field))
-          s.add!(cm_list_id)
-        rescue RuntimeError
-        end
-      end
-
-      after_destroy do |record|
-        begin
-          Campaigning::Subscriber.unsubscribe!(record.send(email_field), cm_list_id)
-        rescue RuntimeError
-        end
+    after_create do |record|
+      begin
+        s = Campaigning::Subscriber.new(record.send(email_field))
+        s.add!(cm_list_id)
+      rescue RuntimeError
       end
     end
 
-    private
-      def cm_list_id
-        YAML::load_file(File.join(RAILS_ROOT, "config/campaign_monitor_subscriber_config.yml"))['list_id']
+    after_destroy do |record|
+      begin
+        Campaigning::Subscriber.unsubscribe!(record.send(email_field), cm_list_id)
+      rescue RuntimeError
       end
+    end
   end
+
+  private
+    def cm_list_id
+      YAML::load_file(File.join(RAILS_ROOT, "config/campaign_monitor_subscriber_config.yml"))['list_id']
+    end
 end
 
-class ActiveRecord::Base
-  include CampaignMonitorSubscriber
-end
+ActiveRecord::Base.extend(CampaignMonitorSubscriber)
