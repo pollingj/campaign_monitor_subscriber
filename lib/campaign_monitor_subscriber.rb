f67f63ab17acc042b92e249a9b5137628d7e795d
Łukasz Anwajler <lukasz@anwajler.com>
2011-07-19 10:38:41 +0200

    Can subscribe with custom fields.

--- lib/campaign_monitor_subscriber.rb
+++ lib/campaign_monitor_subscriber.rb
@@ -3,12 +3,11 @@ module CampaignMonitorSubscriber
   CM_CONFIG = YAML::load_file(File.join("config/campaign_monitor_subscriber_config.yml"))
   ::CAMPAIGN_MONITOR_API_KEY = CM_CONFIG['api_key']  
 
-  def subscribe_me_using(email_field)
+  def subscribe_me_using(email_field, custom_fields={})
     return if CM_CONFIG[::Rails.env] == false
-
     after_create do |record|
       begin
-        s = Campaigning::Subscriber.new(record.send(email_field))
+        s = Campaigning::Subscriber.new(record.send(email_field), custom_fields.map{|k,v| {k => record.send(v)}})
         s.add!(CM_CONFIG['list_id'])
       rescue RuntimeError
       end
