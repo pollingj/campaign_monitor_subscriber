20ffc9f087bfb4263db8c44add5e94e9ceee1d5f
John Polling <john@theled.co.uk>
2012-11-30 17:58:57 +0000

    Added in after_save as I need that

--- lib/campaign_monitor_subscriber/subscription_hooks.rb
+++ lib/campaign_monitor_subscriber/subscription_hooks.rb
@@ -40,6 +40,15 @@ module CampaignMonitorSubscriber
         end
       end
 
+      after_save do |record|
+        @@log.debug "\n* Updating '#{record.cms_email}' from CM"
+        
+        begin
+          s = CreateSend::Subscriber.new(cms_config.list.id, record.cms_email)
+          s.update(record.cms_email, record.cms_name, record.cms_custom_fields, false)
+        rescue CreateSend::CreateSendError => err
+          @@log.info err.message
+        end
     end
   end
 end
